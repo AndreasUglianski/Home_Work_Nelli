83349c3c5ffead1819e3a6f63ce75ec934e2359e
AndreasUglianski <a.uglyansky@gmail.com>
2022-10-30 10:54:29 +0100

    project started

---
+++ HW7_react_workers_cards/workers/src/data/workers.js
@@ -0,0 +1,78 @@
+export const workers = [
+	{
+		id: 1,
+		firstname: 'Irina',
+		lastname: 'Malkova',
+		position: 'Projact manager',
+		in_office: false,
+		avatar: '../../media/face_3.jpeg',
+		tasks: [
+			{
+				id: 1,
+				title: 'Check shedule',
+			},
+			{
+				id: 2,
+				title: 'Drink coffee',
+			},
+			{
+				id: 3,
+				title: 'Fire someone',
+			},
+		],
+	},
+	{
+		id: 2,
+		firstname: 'Olga',
+		lastname: 'Petrova',
+		position: 'CEO',
+		in_office: false,
+		avatar: '../../media/face_2.jpeg',
+		tasks: [
+			{
+				id: 1,
+				title: 'Check shedule',
+			},
+			{
+				id: 2,
+				title: 'Drink coffee',
+			},
+			{
+				id: 3,
+				title: 'Fire someone',
+			},
+		],
+	},
+	{
+		id: 3,
+		firstname: 'Andy',
+		lastname: 'Shulz',
+		position: 'Developer',
+		in_office: true,
+		avatar: '../../media/face_1.jpeg',
+		tasks: [
+			{
+				id: 1,
+				title: 'Check shedule',
+				done: true,
+			},
+			{
+				id: 2,
+				title: 'Drink coffee',
+				done: true,
+			},
+			{
+				id: 3,
+				title: 'Fire someone',
+				done: false,
+			},
+		],
+	},
+	{
+		id: 4,
+		firstname: 'Klava',
+		lastname: 'Cokka',
+		position: 'Java Developer',
+		in_office: true,
+	},
+];
